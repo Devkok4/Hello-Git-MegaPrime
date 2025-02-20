11a74e004749963ccab7a4515b5368af410131f5
Oskar <oskarcermeno1@gmail.com>
2025-02-19 18:27:09 -0600

    Setters y getters clase Libro

--- Books.py
+++ Books.py
@@ -4,3 +4,21 @@ class Book:
         self.idBook=idBook
         self.catBook=catBook
     
+    def getNameBook(self):
+        return self.namBook
+    
+    def getCategBook(self):
+        return self.catBook
+    
+    def getIdBook(self):
+        return self.idBook
+    
+    def setNameBook(self, nameBook):
+        self.namBook= nameBook
+    
+    def setCategBook(self, categBook):
+        self.catBook= categBook
+    
+    def setIdBook(self, idBook):
+        self.idBook= idBook
+    
